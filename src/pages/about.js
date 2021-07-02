a48c3288f15a4950872168db837a4aeb5441ca32
Matheus Carvalho <51411908+mathxx-dev@users.noreply.github.com>
2021-07-02 18:50:47 -0300

    First commit

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './components/header';
+
+class About extends React.Component {
+    render() {
+        return(
+            <div>
+                <Header/>
+            </div>
+        )
+    }
+}
+export default About;
---
+++ src/pages/home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './components/header';
+import Footer from './components/footer';
+
+class Homepage extends React.Component {
+    render() {
+        return(
+            <div>
+                <Header/>
+            </div>
+        )
+    }
+}
+export default Homepage;
