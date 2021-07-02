7f24fc486dcd0f0079f785b05d081b8433d62ac9
Matheus Carvalho <51411908+mathxx-dev@users.noreply.github.com>
2021-07-02 18:50:16 -0300

    First commit

---
+++ src/pages/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Header extends React.Component {
+    render() {
+        return(
+            <div className = 'casa'>
+
+            </div>
+        )
+    }
+}
+export default Header;
